0385d9792059c73f6fbe65c0c65c5d26084afb8c
Jin Cai <cjon72@outlook.com>
2025-06-16 18:36:31 +0800

    modify example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
 use efjson::{
-  ParserOption,
+  JsonValue, ParserOption,
   deserialize::{JsonRawString, JsonRawToken, deserialize},
   event_parser::{EventObjectReceiver, EventParser, EventReceiver},
   stream_parser::StreamParser,
@@ -25,7 +25,7 @@ fn perf() {
   let x = StreamParser::parse(ParserOption::default(), &s);
   println!("{} {}", s.len(), x.unwrap().len());
   let duration = start.elapsed();
-  println!("运行时间: {:?}", duration);
+  println!("Time: {:?}", duration);
 }
 
 const SRC: &'static str = r#"{
@@ -67,42 +67,75 @@ fn test_deserializer() {
     deserialize::<()>(ParserOption::all(), "null")
   );
   println!(
-    "{:?}\t{:?}\t{:?}\t{:?}",
-    deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4]"),
-    deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4,]"),
-    deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,]"),
-    deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4,5]")
+    "{:?}",
+    vec![
+      deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4]"),
+      deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4,]"),
+      deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,]"),
+      deserialize::<[i32; 4]>(ParserOption::all(), "[1,2,3,4,5]")
+    ]
   );
   println!(
     "{:?}",
     deserialize::<HashMap<String, Option<i32>>>(ParserOption::all(), "{'a':1,'b':null,}")
   );
 
-  println!("{:?}", deserialize::<JsonRawToken>(ParserOption::all(), r#"{'a':12,b:[13,14]}"#));
+  {
+    println!("[");
+    for token in
+      deserialize::<JsonRawToken>(ParserOption::all(), r#"{'a':12,b:[13,14]}"#).unwrap().tokens
+    {
+      println!("    {:?}", token);
+    }
+    println!("]");
+  }
 
-  print!("{:?}\t", deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12]"#));
-  print!("{:?}\t", deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12,]"#));
-  print!("{:?}\t", deserialize::<(String, i32)>(ParserOption::all(), r#"[]"#));
-  print!("{:?}\t", deserialize::<(String, i32)>(ParserOption::all(), r#"["a",]"#));
-  print!("{:?}\t", deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12,13]"#));
-  print!("\n");
+  println!(
+    "{:?}\t",
+    vec![
+      deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12]"#),
+      deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12,]"#),
+      deserialize::<(String, i32)>(ParserOption::all(), r#"[]"#),
+      deserialize::<(String, i32)>(ParserOption::all(), r#"["a",]"#),
+      deserialize::<(String, i32)>(ParserOption::all(), r#"["a",12,13]"#)
+    ]
+  );
 
   println!(
     "{:?}",
     deserialize::<HashMap<String, JsonRawString>>(
       ParserOption::all(),
-      r#"{'a':1.2e3,'b':null,'c':[13,14],'d':{a:12,},}"#
+      r#"{'a':1.2e3,'b':null,'c':"str",'d':[13,14],'e':{a:12,},}"#
     )
+    .unwrap()
+    .iter()
+    .map(|(k, v)| (k, &v.json))
+    .collect::<HashMap<_, _>>()
   );
   println!(
     "{:?}",
-    deserialize::<Vec<JsonRawString>>(ParserOption::all(), r#"[1.2e3,null,[13,14,],{a:12,},]"#)
+    deserialize::<Vec<JsonRawString>>(
+      ParserOption::all(),
+      r#"[1.2e3,null,"str",[13,14,],{a:12,},]"#
+    )
+    .unwrap()
+    .iter_mut()
+    .map(|x| &x.json)
+    .collect::<Vec<_>>()
+  );
+  println!(
+    "{:?}",
+    deserialize::<JsonValue>(ParserOption::all(), r#"[1.2e3,null,"str",[13,14,],{"a":12,},]"#)
+      .unwrap()
   );
 }
 
 fn main() {
+  println!("======Stream Parser======");
   test_stream();
-  test_event();
-  test_deserializer();
   perf();
+  println!("======Event Parser======");
+  test_event();
+  println!("======Deserializer======");
+  test_deserializer();
 }
