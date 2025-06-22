d16aac71fffb73ba6b04c51888a44b0e90a3704e
Jin Cai <cjon72@outlook.com>
2025-06-22 21:48:17 +0800

    [refacto, fix] use struct helper for derive macro to avoid the deserializer is not `Unpin`

--- efjson_derive/src/lib.rs
+++ efjson_derive/src/lib.rs
@@ -11,179 +11,182 @@ pub fn derive_answer_fn(input: proc_macro::TokenStream) -> proc_macro::TokenStre
     syn::Data::Enum(_) => panic!("TODO: enums are not supported"),
     syn::Data::Struct(item) => item,
   };
-  let span = proc_macro2::Span::mixed_site();
-  let mut stream = TokenStream::new();
-  let fields = &data.fields;
   let ident = &input.ident;
 
-  // define the value receiver enum, and impl `Deserializer` for it
-  let enum_name = format_ident!("__EfjsonEnum_{}", input.ident, span = span);
+  let span = proc_macro2::Span::mixed_site();
+  let mut stream = TokenStream::new();
+
+  let fields = &data.fields;
+  let field_cnt = fields.len();
+  if field_cnt == 0 {
+    panic!("the struct must contain at least a member");
+  }
+
+  let union_ident = format_ident!("__EfjsonUnion_{}", input.ident, span = span);
+  let struct_ident = format_ident!("__EfjsonStruct_{}", input.ident, span = span);
+
   stream.extend({
-    let mut enum_members = quote_spanned! {span=>};
+    let mut content = quote_spanned! {span=>};
     for field in fields {
       let ident = &field.ident;
       let typ = &field.ty;
-      enum_members.extend(quote_spanned! {span=>
-        #ident(<#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::DefaultDeserializer, *mut Option<#typ>)
+      content.extend(quote_spanned! {span=>
+        #ident: ::std::mem::ManuallyDrop<
+          <#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::DefaultDeserializer
+        >
       });
-      enum_members.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+      content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
     }
+
     quote_spanned! {span=>
+      #[allow(non_camel_case_types)] union #union_ident { #content }
       #[allow(non_camel_case_types)]
-      enum #enum_name { #enum_members }
-    }
-  });
-  stream.extend({
-    let mut receiver_content = quote_spanned! {span=>};
-    for field in fields {
-      let ident = &field.ident;
-      receiver_content.extend(quote_spanned! {span=>
-        Self::#ident(ref mut subr, ptr) => match subr.feed_token(token)? {
-          efjson::deserialize::DeserResult::Complete(ret) => {
-            let _ = unsafe { std::mem::replace(ptr.as_mut().unwrap_unchecked(), Some(ret)) };
-            efjson::deserialize::DeserResult::Complete(())
-          }
-          efjson::deserialize::DeserResult::CompleteWithRollback(ret) => {
-            let _ = unsafe { std::mem::replace(ptr.as_mut().unwrap_unchecked(), Some(ret)) };
-            efjson::deserialize::DeserResult::CompleteWithRollback(())
-          }
-          efjson::deserialize::DeserResult::Continue => efjson::deserialize::DeserResult::Continue,
-        }
-      });
-      receiver_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
-    }
-
-    quote_spanned! {span=>
-      impl ::efjson::deserialize::Deserializer<()> for #enum_name {
-        fn feed_token(
-          &mut self, token: ::efjson::stream_parser::Token
-        ) -> ::std::result::Result<::efjson::deserialize::DeserResult<()>, ::efjson::deserialize::DeserError>
-        {
-          Ok(match self { #receiver_content })
-        }
+      struct #struct_ident {
+        target: ::std::mem::MaybeUninit<#ident>,
+        subreceiver: ::std::mem::MaybeUninit<#union_ident>,
+        index: usize,
+        flag: [bool; #field_cnt],
       }
     }
   });
 
-  // define the struct receiver, and impl `ObjectReceiverTrait` for it
-  let struct_receiver_name = format_ident!("__EfjsonReceiver_{}", input.ident, span = span);
   stream.extend({
-    let mut struct_receiver_fields = quote_spanned! {span=>};
-    for field in fields {
-      let ident = &field.ident;
-      let ty = &field.ty;
-      struct_receiver_fields.extend(quote_spanned! {span=>
-        #ident: Option<#ty>
-      });
-      struct_receiver_fields.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
-    }
-    quote_spanned! {span=>
-      #[derive(Default)]
-      struct #struct_receiver_name {
-        #struct_receiver_fields
-      }
-    }
-  });
-  stream.extend({
-    let mut value_receivers = quote_spanned! {span=>};
-    for field in fields {
+    let mut start_content = quote_spanned! {span=>};
+    for (idx, field) in fields.iter().enumerate() {
       let ident = &field.ident;
       let typ = &field.ty;
-      value_receivers.extend(quote_spanned! {span=>
-        ::core::stringify!(#ident) => Ok(#enum_name::#ident(
-          <#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::default_deserializer(),
-          &mut self.#ident,
-        ))
+      start_content.extend(quote_spanned! {span=>
+        ::std::stringify!(#ident) => {
+          if self.flag[#idx] {
+            Err(format!("repeated key: {}", key).into())
+          } else {
+            unsafe {
+              ::std::ptr::write(&mut (*self.subreceiver.as_mut_ptr()).#ident
+                as &mut <#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::DefaultDeserializer,
+                <#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::default_deserializer())
+            };
+            self.index = #idx;
+            Ok(())
+          }
+        }
       });
-      value_receivers.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+      start_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
     }
 
-    let mut return_content = quote_spanned! {span=>};
-    for field in fields {
+    let mut feed_content = quote_spanned! {span=>};
+    for (idx, field) in fields.iter().enumerate() {
       let ident = &field.ident;
-      return_content.extend(quote_spanned! {span=>
-        #ident: self.#ident.take().ok_or_else(|| format!("missing field: {}", ::core::stringify!(#ident)))?
+      let typ = &field.ty;
+      feed_content.extend(quote_spanned! {span=>
+        #idx => match ::efjson::deserialize::Deserializer::feed_token(
+          unsafe {
+            &mut self.subreceiver.assume_init_mut().#ident
+              as &mut <#typ as ::efjson::deserialize::DefaultDeserializable<#typ>>::DefaultDeserializer
+          },
+          token,
+        )? {
+          ::efjson::deserialize::DeserResult::Complete(r) => {
+            unsafe { ::std::ptr::write(&mut (*self.target.as_mut_ptr()).#ident, r) };
+            self.flag[#idx] = true;
+            self.index = usize::max_value();
+            ::efjson::deserialize::DeserResult::Complete(())
+          }
+          ::efjson::deserialize::DeserResult::CompleteWithRollback(r) => {
+            unsafe { ::std::ptr::write(&mut (*self.target.as_mut_ptr()).#ident, r) };
+            self.flag[#idx] = true;
+            self.index = usize::max_value();
+            ::efjson::deserialize::DeserResult::CompleteWithRollback(())
+          }
+          ::efjson::deserialize::DeserResult::Continue => ::efjson::deserialize::DeserResult::Continue
+        }
       });
-      return_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+      feed_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+    }
+
+    let mut end_content = quote_spanned! {span=>};
+    for (idx, field) in fields.iter().enumerate() {
+      let ident = &field.ident;
+      end_content.extend(quote_spanned! {span=>
+        if !self.flag[#idx] { list.push(::std::stringify!(#ident)); }
+      });
     }
 
     quote_spanned! {span=>
-      impl
-        ::efjson::deserialize::ObjectReceiverTrait<
-          ::std::string::String,
-          (),
-          #ident,
-          <::std::string::String as ::efjson::deserialize::DefaultDeserializable<
-            ::std::string::String,
-          >>::DefaultDeserializer,
-          #enum_name,
-        > for #struct_receiver_name
-      {
-        fn create_key(
-          &mut self,
-        ) -> ::std::result::Result<
-          <::std::string::String as ::efjson::deserialize::DefaultDeserializable<
-            ::std::string::String,
-          >>::DefaultDeserializer,
-          ::efjson::deserialize::DeserError,
-        > {
-          Ok(<::std::string::String as ::efjson::deserialize::DefaultDeserializable<
-            ::std::string::String,
-          >>::default_deserializer())
-        }
-
-        fn create_value(
-          &mut self,
-          key: &::std::string::String,
-        ) -> ::std::result::Result<#enum_name, ::efjson::deserialize::DeserError> {
-          match key as &::std::primitive::str {
-            #value_receivers
-            _ => Err(format!("unxepcted key: {}", key).into()),
+      impl ::efjson::deserialize::StructHelperReceiverTrait<#ident> for #struct_ident {
+        fn start_value(&mut self, key: &str) -> ::std::result::Result<(), ::efjson::deserialize::DeserError> {
+          match key {
+            #start_content
+            _ => ::std::result::Result::Err(::std::format!("unxepcted key: {}", key).into()),
           }
         }
-
-        fn set(
+        fn feed_value(
           &mut self,
-          _key: ::std::string::String,
-          _value: (),
-        ) -> ::std::result::Result<(), ::efjson::deserialize::DeserError> {
-          Ok(())
-        }
-
-        fn end(&mut self) -> ::std::result::Result<#ident, ::efjson::deserialize::DeserError> {
-          Ok(#ident {
-            #return_content
+          token: ::efjson::stream_parser::Token,
+        ) -> ::std::result::Result<::efjson::deserialize::DeserResult<()>, ::efjson::deserialize::DeserError> {
+          Ok(match self.index {
+            #feed_content
+            _ => unsafe { ::std::hint::unreachable_unchecked() },
           })
         }
+        fn end(&mut self) -> ::std::result::Result<#ident, ::efjson::deserialize::DeserError> {
+          if self.flag.iter().all(|f| *f) {
+            self.flag.fill(false);
+            Ok(unsafe { self.target.assume_init_read() })
+          } else {
+            let mut list: Vec<&str> = Vec::new();
+            #end_content
+            Err(::std::format!("missing required fileds: {}", list.join(", ")).into())
+          }
+        }
       }
     }
   });
 
-  // impl `DefaultDeserializable` for the struct
   stream.extend({
-    let mut field_content = quote_spanned! {span=>};
-    for field in fields {
+    let mut deser_drop_content = quote_spanned! {span=>};
+    for (idx, field) in fields.iter().enumerate() {
       let ident = &field.ident;
-      field_content.extend(quote_spanned! {span=>
-        #ident: ::std::option::Option::None
+      deser_drop_content.extend(quote_spanned! {span=>
+        #idx => unsafe { ::std::mem::ManuallyDrop::drop(&mut self.subreceiver.assume_init_mut().#ident) }
       });
-      field_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+      deser_drop_content.append(proc_macro2::Punct::new(',', proc_macro2::Spacing::Alone));
+    }
+
+    let mut val_drop_content = quote_spanned! {span=>};
+    for (idx, field) in fields.iter().enumerate() {
+      let ident = &field.ident;
+      val_drop_content.extend(quote_spanned! {span=>
+        if self.flag[#idx] {
+          unsafe { ::std::ptr::drop_in_place(&mut (*self.target.as_mut_ptr()).#ident) };
+        }
+      });
     }
 
     quote_spanned! {span=>
+      impl ::std::ops::Drop for #struct_ident {
+        fn drop(&mut self) {
+          match self.index {
+            #deser_drop_content
+            _ => {}
+          }
+          #val_drop_content
+        }
+      }
+    }
+  });
+
+  stream.extend({
+    quote_spanned! {span=>
       impl ::efjson::deserialize::DefaultDeserializable<#ident> for #ident {
-        type DefaultDeserializer = ::efjson::deserialize::ObjectReceiverDeserializer<
-          ::std::string::String,
-          (),
-          #ident,
-          #struct_receiver_name,
-          <::std::string::String as ::efjson::deserialize::DefaultDeserializable<
-            ::std::string::String,
-          >>::DefaultDeserializer,
-          #enum_name,
-        >;
+        type DefaultDeserializer
+          = ::efjson::deserialize::StructHelperReceiverDeserializer<#ident, #struct_ident>;
         fn default_deserializer() -> Self::DefaultDeserializer {
-          efjson::deserialize::create_object_deserializer(#struct_receiver_name { #field_content })
+          ::efjson::deserialize::create_struct_helper_deserializer(#struct_ident {
+            flag: ::std::default::Default::default(),
+            index: usize::max_value(),
+            target: ::std::mem::MaybeUninit::uninit(),
+            subreceiver: ::std::mem::MaybeUninit::uninit(),
+          })
         }
       }
     }
